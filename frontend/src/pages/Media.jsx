cb6ff26bd5c52f18924a19e3cfc45acd65a2a273
Ruchapatil03 <rucha14patil@gmail.com>
2023-10-21 11:58:54 +0530

    Media base struct

--- frontend/src/pages/Media.jsx
+++ frontend/src/pages/Media.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import Navbar from "../components/Navbar";
+import Footer from "../components/footer";
+import "../stylesheets/Media.css";
 const Media = () => {
-	return <div>Media</div>;
+	return (
+		<>
+		<section>
+			<title>Media</title>
+			<Navbar/>
+		</section>
+		</>
+	);
 };
 
 export default Media;
