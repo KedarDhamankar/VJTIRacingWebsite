81a98552972e1418cdc11b6a01840bc4b04e91df
KedarDhamankar <kedard2004@gmail.com>
2023-10-29 19:30:18 +0530

    Crowdfunding form works

--- frontend/src/pages/Crowdfunding.jsx
+++ frontend/src/pages/Crowdfunding.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import { useRef, React } from "react";
 import {
     PieChart,
     Pie,
@@ -21,6 +21,7 @@ import im4 from "../assets/Crowdfunding/car.png";
 import qr from "../assets/Crowdfunding/scan.png";
 import bg1 from "../assets/SponsorUs/3.png";
 import { useNavigate } from "react-router-dom";
+import { useReducer } from "react";
 
 const Crowdfunding = () => {
     const navigate = useNavigate();
@@ -62,20 +63,18 @@ const Crowdfunding = () => {
     //         { method: "POST", body: formData }
     //     );
     // }
-
-    window.onload = function () {
+    const imgdata = useRef();
+    window.onload = function hello(e) {
         e.preventDefault();
         // This line declares a variable called "url" and assigns it a value of "Api_Endpoint_Url"
         let url =
-            "https://script.google.com/macros/s/AKfycbw9K6g5jHbFjbKwN4Y51o60AQPkaUjf35hWQIqe-u2cYVq1J-WyxGnIL4CGhy3df7nUjw/exec";
+            "https://script.google.com/macros/s/AKfycbzSX9ljLiSEefS9EswAUDpIhDDmC6B_2qDzwF3-ua_AeRQN0m41xUoPCUdNxso3utwXEA/exec";
         // This line declares a variable called "file" and assigns it the value of the first input element on the page
         let file = document.getElementById("screenshot");
         // This line declares a variable called "img" and assigns it the value of the first image element on the page
         let img = document.getElementById("screenshotimg");
-
         // This line adds an event listener to the "change" event of the "file" input element
         file.addEventListener("change", () => {
-            // e.preventDefault();
             // This line creates a new FileReader object called "fr"
             let fr = new FileReader();
             // This line adds an event listener to the "loadend" event of the FileReader object
@@ -87,20 +86,22 @@ const Crowdfunding = () => {
                 // This line splits the "res" variable into an array, using the string "base64," as the separator, and assigns the second element to a variable called "spt"
                 let spt = res.split("base64,")[1];
                 // This line creates an object called "obj" with three properties: "base64", "type", and "name"
-                let obj = {
+                var obj = {
                     base64: spt,
                     type: file.files[0].type,
                     name: file.files[0].name,
                 };
+                imgdata.current = obj;
+
+                // fetch(url, {
+                //     method: "POST",
+                //     body: JSON.stringify(obj),
+                // })
+                //     // This line waits for the response from the server and converts it to text
+                //     .then((r) => r.text())
+                //     // This line logs the response data to the console
+                //     .then((data) => console.log(data));
                 // This line sends a POST request to the URL specified in the "url" variable, with the "obj" object as the request body
-                fetch(url, {
-                    method: "POST",
-                    body: JSON.stringify(obj),
-                })
-                    // This line waits for the response from the server and converts it to text
-                    .then((r) => r.text())
-                    // This line logs the response data to the console
-                    .then((data) => console.log(data));
             });
             // This line reads the selected file as a data URL
             fr.readAsDataURL(file.files[0]);
@@ -108,15 +109,74 @@ const Crowdfunding = () => {
     };
 
     function handleSubmit(e) {
-        const formEle = document.querySelector("form");
         e.preventDefault();
-        console.log("Submitted");
-        const formData = new FormData(formEle);
-        fetch(
-            "https://script.google.com/macros/s/AKfycbwxqCBrCMk65xL4XauavyDYWT1p9IKFV59TNv8kl3Z6b2RSYxyk-LT6QT_ETj8t1JrV/exec",
-            { method: "POST", body: formData }
-        );
+        const formEle = document.getElementById("forms");
+        let formData = new FormData(formEle);
+        var object = {};
+        formData.forEach(function (value, key) {
+            object[key] = value;
+        });
+        // let dict = formData;
+        let requestBody = {
+            imgdata: imgdata.current,
+            dict: object,
+        };
+
+        let url =
+            "https://script.google.com/macros/s/AKfycbzz5RtoPVqMwRso717bEhwIkxsGXlY5cIrvi1FusVcDD0zblgCwS9oRqgHt7PrQWgEqcQ/exec";
+        fetch(url, {
+            method: "POST",
+            body: JSON.stringify(requestBody),
+        })
+            // This line waits for the response from the server and converts it to text
+            .then((r) => r.text())
+            // This line logs the response data to the console
+            .then((data) => console.log(data));
     }
+
+    // function imgupload() {
+
+    // let imgdata = useRef();
+    // window.onload = function () {
+    //     let url = "Api_Endpoint_Url";
+    //     // This line declares a variable called "file" and assigns it the value of the first input element on the page
+    //     let file = document.getElementById("screenshot");
+    //     // This line declares a variable called "img" and assigns it the value of the first image element on the page
+    //     let img = document.getElementById("screenshotimg");
+    //     // This line adds an event listener to the "change" event of the "file" input element
+    //     file.addEventListener("change", () => {
+    //         // This line creates a new FileReader object called "fr"
+    //         let fr = new FileReader();
+    //         // This line adds an event listener to the "loadend" event of the FileReader object
+    //         fr.addEventListener("loadend", () => {
+    //             // This line declares a variable called "res" and assigns it the result of the FileReader object
+    //             let res = fr.result;
+    //             // This line sets the "src" attribute of the "img" element to the value of "res"
+    //             img.src = res;
+    //             // This line splits the "res" variable into an array, using the string "base64," as the separator, and assigns the second element to a variable called "spt"
+    //             let spt = res.split("base64,")[1];
+    //             // This line creates an object called "obj" with three properties: "base64", "type", and "name"
+    //             let obj = {
+    //                 base64: spt,
+    //                 type: file.files[0].type,
+    //                 name: file.files[0].name,
+    //             };
+    //             imgdata.current = obj;
+    //             // This line sends a POST request to the URL specified in the "url" variable, with the "obj" object as the request body
+    //             // fetch(url, {
+    //             //     method: "POST",
+    //             //     body: JSON.stringify(obj),
+    //             // })
+    //             // This line waits for the response from the server and converts it to text
+    //             //         .then((r) => r.text())
+    //             //         // This line logs the response data to the console
+    //             //         .then((data) => console.log(data));
+    //         });
+    //         // This line reads the selected file as a data URL
+    //         fr.readAsDataURL(file.files[0]);
+    //         // imgdata.current.files[0];
+    //     });
+    // };
 
     return (
         <>
@@ -511,7 +571,11 @@ const Crowdfunding = () => {
                             CROWDFUNDING CAMPAIGN!
                         </span>
                     </div>
-                    <form className="fform" onSubmit={(e) => handleSubmit(e)}>
+                    <form
+                        className="fform"
+                        onSubmit={(e) => handleSubmit(e)}
+                        id="forms"
+                    >
                         <div>
                             <label>Email</label>
                             <br />
@@ -530,7 +594,12 @@ const Crowdfunding = () => {
                         <div>
                             <label>Contact Number</label>
                             <br />
-                            <input name="Number" type="text" required />
+                            <input
+                                name="Number"
+                                type="tel"
+                                required
+                                pattern="[0-9]{10}"
+                            />
                         </div>
                         {/* <div>
                                 <label>Team you would like to support</label>
